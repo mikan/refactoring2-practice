35b221b0508252194c38952a7127bc82d81a67ad
Yutaka Kato <mikan@aomikan.org>
2022-12-05 17:54:36 +0900

    move playFor and amountFor

--- ch01/06-splitting-the-phases/statement.js
+++ ch01/06-splitting-the-phases/statement.js
@@ -1,17 +1,15 @@
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
-}
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  return renderPlainText(statementData, plays);
 
-function renderPlainText(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
-  for (let perf of invoice.performances) {
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    } seats)\n`;
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    return result;
   }
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
-  return result;
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
@@ -19,7 +17,7 @@ function renderPlainText(invoice, plays) {
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -34,15 +32,27 @@ function renderPlainText(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+        throw new Error(`Unknown type: ${aPerformance.play.type}`);
     }
     return result;
   }
+}
+
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
+  for (let perf of data.performances) {
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`;
+  }
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return result;
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
@@ -57,7 +67,7 @@ function renderPlainText(invoice, plays) {
 
   function totalVolumeCredits() {
     let volumeCredits = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       volumeCredits += volumeCreditsFor(perf);
     }
     return volumeCredits;
@@ -65,8 +75,8 @@ function renderPlainText(invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
+    for (let perf of data.performances) {
+      result += perf.amount;
     }
     return result;
   }
