1182169e3e39a7e2d1f5db7e0c19ccb49fd01fe1
Yutaka Kato <mikan@aomikan.org>
2022-12-05 17:01:18 +0900

    WIP ch01s04 p21

--- ch01/04-decomposing-the-statement-function/statement2.js
+++ ch01/04-decomposing-the-statement-function/statement2.js
@@ -1,21 +1,13 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         // 注文の内訳を出力
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
     function playFor(aPerformance) {
@@ -49,6 +41,22 @@ function statement(invoice, plays) {
         result += Math.max(aPerformance.audience - 30, 0);
         if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
         return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber / 100);
+    }
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+        return volumeCredits;
     }
 }
 
