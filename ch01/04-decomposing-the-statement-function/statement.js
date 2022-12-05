0aeabe830d3db7e40c9a7820fca876cb66f3663c
Yutaka Kato <mikan@aomikan.org>
2022-12-05 17:05:28 +0900

    complete ch01s04

--- ch01/04-decomposing-the-statement-function/statement.js
+++ ch01/04-decomposing-the-statement-function/statement.js
@@ -1,12 +1,9 @@
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        // 注文の内訳を出力
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
@@ -57,6 +54,14 @@ function statement(invoice, plays) {
             volumeCredits += volumeCreditsFor(perf);
         }
         return volumeCredits;
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
     }
 }
 
