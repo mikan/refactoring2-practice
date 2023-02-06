743c15428e16a0cb1c8f90f74bf6b1b5783a8691
Yutaka Kato <mikan@aomikan.org>
2023-02-06 23:11:57 +0900

    ch04s03 separate files

---
+++ ch04/03-a-first-test/province.test.js
@@ -0,0 +1,11 @@
+import assert from "assert";
+import { Province } from "../02-sample-code-to-test/province.js";
+import { sampleProvinceData } from "../02-sample-code-to-test/sample.js";
+
+
+describe("province", function () {
+  it("shortfall", function () {
+    const asia = new Province(sampleProvinceData());
+    assert.equal(asia.shortfall, 5);
+  });
+});
