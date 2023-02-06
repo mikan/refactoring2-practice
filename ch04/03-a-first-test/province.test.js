a0db4f107b43a8a3eae0c366d21db10a024bdd97
Yutaka Kato <mikan@aomikan.org>
2023-02-06 23:13:57 +0900

    ch04s03 format

--- ch04/03-a-first-test/province.test.js
+++ ch04/03-a-first-test/province.test.js
@@ -2,7 +2,6 @@ import assert from "assert";
 import { Province } from "../02-sample-code-to-test/province.js";
 import { sampleProvinceData } from "../02-sample-code-to-test/sample.js";
 
-
 describe("province", function () {
   it("shortfall", function () {
     const asia = new Province(sampleProvinceData());
