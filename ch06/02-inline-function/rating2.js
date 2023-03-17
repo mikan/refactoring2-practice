dcbbddcd1fa7f2e0b03ebaf9829768d4d1676a42
Yutaka Kato <mikan@aomikan.org>
2023-03-17 12:21:23 +0900

    ch06s02 fmt

--- ch06/02-inline-function/rating2.js
+++ ch06/02-inline-function/rating2.js
@@ -1,5 +1,5 @@
 function getRating(driver) {
-  return (driver.numberOfLateDeliveries > 5) ? 2 : 1;
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 console.log(getRating({ numberOfLateDeliveries: 10 }));
--- ch06/02-inline-function/rating4.js
+++ ch06/02-inline-function/rating4.js
@@ -1,5 +1,5 @@
 function getRating(aDriver) {
-  return (aDriver.numberOfLateDeliveries > 5) ? 2 : 1;
+  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 console.log(getRating({ numberOfLateDeliveries: 10 }));
